ca7e60fde676b5a9fb03352e5d5318cc0b8cb3da
Daniel Wei <danielwei02@hotmail.com>
2019-11-25 13:21:51 -0500

    Add project files.

---
+++ src/filter.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#ifndef PCH_H
+#define PCH_H
+
+#include <winsock2.h>
+#include <in6addr.h>
+#include <tchar.h>
+#include <ws2ipdef.h>
+#include <IPHlpApi.h>
+#include <Mstcpip.h>
+#include <conio.h>
+#include <WinDNS.h>
+
+#include <memory>
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <thread>
+#include <limits>
+#include <atomic>
+#include <string>
+#include <functional>
+#include <vector>
+#include <cassert>
+#include <array>
+#include <map>
+#include <cctype>
+#include <shared_mutex>
+
+#include "..\lib\ndisapi\include\ndisapi.h"
+#include "..\lib\ndisapi\include\Common.h"
+#include "..\lib\ndisapi\ndisapi\iphlp.h"
+
+#endif //PCH_H
