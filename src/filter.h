80bad6d7842f76ae47faaad4ccc61851df24ec0d
Daniel Wei <danielwei02@hotmail.com>
2019-12-17 14:16:45 -0500

    Creating blacklist structure

--- src/filter.h
+++ src/filter.h
@@ -1,33 +1,51 @@
 #pragma once
 
-
-
-#ifndef PCH_H
-#define PCH_H
-
-#include <winsock2.h>
-#include <in6addr.h>
-#include <tchar.h>
-#include <ws2ipdef.h>
-#include <IPHlpApi.h>
-#include <Mstcpip.h>
-#include <conio.h>
-#include <WinDNS.h>
-
-#include <memory>
 #include <iostream>
-#include <iomanip>
-#include <chrono>
-#include <thread>
-#include <limits>
-#include <atomic>
-#include <string>
-#include <functional>
-#include <vector>
-#include <cassert>
-#include <array>
-#include <map>
-#include <cctype>
-#include <shared_mutex>
+#include "windivert.h"
 
-#endif //PCH_H
+//how urls will be stored
+typedef struct {
+	UINT length;
+	char* domain;
+}URL, *PURL;
+
+//how the blacklist will be stored
+typedef struct {
+	UINT size;
+	UINT length;
+	PURL *url;
+}BLACKLIST, *PBLACKLIST;
+
+PBLACKLIST BlacklistInit() {
+	UINT size = 1024;
+
+	PBLACKLIST blacklist = (PBLACKLIST)malloc(sizeof(BLACKLIST));
+	if (blacklist == NULL) {
+		goto memory_error;
+	}
+
+	blacklist->url = (PURL*)malloc(sizeof(URL));
+	if (blacklist->url == NULL) {
+		goto memory_error;
+	}
+
+	blacklist->size = size; //memory size
+	blacklist->length = 0; //number of urls
+
+	return blacklist;
+memory_error:
+	std::cerr << "Failed to allocate memory";
+	exit(EXIT_FAILURE);
+}
+
+void BlacklistAdd(PBLACKLIST blacklist, char* list) {
+
+
+
+memory_error:
+	std::cerr << "Failed to allocate memory";
+	exit(EXIT_FAILURE);
+}
+
+
+
